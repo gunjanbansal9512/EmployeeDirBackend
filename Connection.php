059aa34a041dd39fb82d09461e736afd43362be3
Gunajn Bansal <39277147+gunjanbansal9512@users.noreply.github.com>
2021-01-31 22:01:54 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,4 +1,4 @@
 <?php
-$db_handle = pg_connect("host = localhost port = 5555 dbname = Employee user = postgres password = root");
+$db_handle = pg_connect("host = ec2-3-231-46-238.compute-1.amazonaws.com port = 5432 dbname = d6bis5v80pvbhg user = wikytnbhzfhggy password = 977504b84432fe7744b204c84112eebcd4c8b68a8eadc8bb10942739a439a992");
 
 ?>
